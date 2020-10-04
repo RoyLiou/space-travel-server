ea3e6af34b9b193b2c67b6e43e765b29566e67e7
RoyLiu <royliu@kdanmobile.com>
2020-10-04 23:28:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Space Travel Server
+
+This project is an example.
+
+used nodejs, nextjs, react, websocket, apollo graphQL, Material UI.
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import 'dotenv';
+
+import { ApolloServer } from 'apollo-server';
+
+import typeDefs from './schema';
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
